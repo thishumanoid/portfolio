dcbe51af4be94c816ca9f2c9e69e3c2fc8548473
himan <logicallhimanshu@gmail.com>
2026-07-13 18:07:53 +0530

    adjdust next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 
-const repoName = "portfolio"
 
 const nextConfig = {
-    basePath: process.env.NODE_ENV === "production" ? `/${repoName}` : "",
-    output: 'export',
-    images: { unoptimized: true },
-    compiler: {
-        removeConsole: process.env.NODE_ENV === "production",
-    },
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio/',
+  output: 'export',
+  images: { unoptimized: true },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
 };
 
 export default nextConfig;
